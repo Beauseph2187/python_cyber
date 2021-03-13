d03d08051a9903b166a2965224103a61e2a46006
Beauseph2187 <71625183+Beauseph2187@users.noreply.github.com>
2021-03-13 16:15:51 -0600

    Learning Range Functions

---
+++ first_numbers.py
@@ -0,0 +1,12 @@
+#Range function to print a range of numbers
+
+for value in range (1, 5):
+    print(value)
+
+#The code will not print the number 5
+#Due to stopping at second value provided.
+
+for value in range (1, 6):
+    print(value)
+
+# Above code is used to print 1-5.
