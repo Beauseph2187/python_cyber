ae6ebb20341857ea2c481971213d0bca8fcca5f5
Beauseph2187 <71625183+Beauseph2187@users.noreply.github.com>
2021-03-12 22:01:29 -0600

    Learned some new things!

--- greet.py
+++ greet.py
@@ -1,1 +1,8 @@
-print("Hello World")
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+message = "Hello Python Crash Course reader!"
+print(message)
---
+++ name_cases.py
@@ -0,0 +1,9 @@
+Bob = "Hello Bob, would you like to learn some Python today?"
+print(Bob)
+name = "Bob Smith"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+quote = "Uncle Iroh says 'Life happens wherever you are, wether you make it or not.'"
+print(quote) 
